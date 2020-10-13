1fd6184f1da221881f6fe848c4a8fe798a6fc67a
Thy <thynguyen2269@gmail.com>
2020-10-13 10:51:41 +0700

    Them Ham tinh HCN

---
+++ header.h
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+
+float cvHCN (float dai, float rong);
+float dtHCN (float dai, float rong);
+
--- main.cpp
+++ main.cpp
@@ -13,5 +13,17 @@ int main(){
 		cout<<"Ket qua chia "<<a/b<<endl;
 	}
 	else cout<<"Khong the chia cho 0"<<endl;
+	
+	//Cv-DtHCN
+	float dai, rong;
+	cout<<"Nhap dai: ";
+	cin>>dai;
+	cout<<"Nhap rong: "
+	cin>>rong;
+	cout<<"Ket qua chi vi HCN: "<<cvHCN(dai,rong)<<endl;
+	cout<<"Ket qua dien tich HCN: "<<dtHCN(dai,rong);
+	
 	return 0;
+	
+	
 }
